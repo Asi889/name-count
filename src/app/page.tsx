529c4877ed62b3fd6e0fb5acac6e2a6fcaddb37c
asi marom <asafmarom89@gmail.com>
2024-03-17 13:35:24 +0200

    Add Suspense to improve component rendering performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/Container";
 import { gernateData } from "@/utils/getAllData";
+import { Suspense } from "react";
 
 export default async function Home() {
   const data = (await gernateData()) ?? {
@@ -10,7 +11,9 @@ export default async function Home() {
   return (
     <div className="text-center w-full h-screen overflow-x-hidden bg-[#C9D9DA]">
       {/* <div className="z-50 pt-20 px-6"> */}
-      <Container data={data.allData} names={data.names} />
+      <Suspense>
+        <Container data={data.allData} names={data.names} />
+      </Suspense>
       {/* </div> */}
     </div>
   );
